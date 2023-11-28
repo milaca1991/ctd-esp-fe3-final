795507107aae7b2558c3c4f292c91e77d1fd6586
camila garcia <milaca1991@hotmail.com>
2023-11-27 22:19:57 -0500

    se añade dentistas a favoritos

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -6,12 +6,30 @@ const Card = ({ name, username, id }) => {
 
   const addFav = ()=>{
     // Aqui iria la logica para agregar la Card en el localStorage
+
+    const currentFavs = JSON.parse(localStorage.getItem('favs')) || [];
+
+    // Verificar si la tarjeta ya está en favoritos
+    const isAlreadyFav = currentFavs.some((fav) => fav.id === id);
+
+    if (!isAlreadyFav) {
+      // Si la tarjeta no está en favoritos, agregarla al array
+      currentFavs.push({ id, name, username });
+      // Guardar el array actualizado en el localStorage
+      localStorage.setItem('favs', JSON.stringify(currentFavs));
+      alert('Añadido a favoritos');
+    } else {
+      alert('Esta tarjeta ya está en favoritos');
+    }
   }
+
+  
 
   return (
     <div className="card">
         {/* En cada card deberan mostrar en name - username y el id */}
         <Link to={`/detail/${id}`}>
+        <img src="/images/doctor.jpg" alt="" id="doc-img"/>
         <h2>{name}</h2>
         <p>Username: {username}</p>
         <p>ID: {id}</p>
@@ -20,6 +38,8 @@ const Card = ({ name, username, id }) => {
         {/* No debes olvidar que la Card a su vez servira como Link hacia la pagina de detalle */}
 
         {/* Ademas deberan integrar la logica para guardar cada Card en el localStorage */}
+
+
         <button onClick={addFav} className="favButton">Add fav</button>
     </div>
   );
--- src/Routes/Favs.jsx
+++ src/Routes/Favs.jsx
@@ -5,12 +5,27 @@ import Card from "../Components/Card";
 
 const Favs = () => {
 
+  const favDentists = JSON.parse(localStorage.getItem('favs')) || [];
+
   return (
     <>
       <h1>Dentists Favs</h1>
       <div className="card-grid">
         {/* este componente debe consumir los destacados del localStorage */}
+        {favDentists.length > 0 ? (
+          favDentists.map((dentist) => (
+            <Card
+              key={dentist.id}
+              id={dentist.id}
+              name={dentist.name}
+              username={dentist.username}
+            />
+          ))
+        ) : (
+          <p>No hay dentistas en favoritos</p>
+        )}
         {/* Deberan renderizar una Card por cada uno de ellos */}
+
       </div>
     </>
   );
